1a6dc11e84eb5bc4e358eefc0334d2f4e3c0cd82
D073535 <moritz.moelders@sap.com>
2023-06-01 13:51:00 +0200

    add sample call

--- src/app/micro-frontend/pages/home-page/home-page.component.ts
+++ src/app/micro-frontend/pages/home-page/home-page.component.ts
@@ -1,25 +1,50 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MicroFrontendService } from '../../services/micro-frontend.service';
+import { DxpLuigiContextService } from '@dxp/ngx-core/luigi';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  pageTitle = 'pipeline-ui';
+    pageTitle = 'pipeline-ui';
 
-  constructor(private microFrontendService: MicroFrontendService) { }
-  
-  counter: Observable<number>;
+    constructor(
+        private microFrontendService: MicroFrontendService,
+        private luigiContextService: DxpLuigiContextService
+    ) {}
 
-  ngOnInit(): void {
-    this.counter = this.microFrontendService.getCounter();
-  }
+    counter: Observable<number>;
+    userId: Subject<string> = new Subject();
 
-  countUp(): void {
-    this.microFrontendService.countUp();
-  }
+    async ngOnInit(): Promise<void> {
+        this.counter = this.microFrontendService.getCounter();
+
+        const context = await this.luigiContextService.getContextAsync();
+
+        const result = await fetch('http://localhost:3000/query', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${context.token}`,
+            },
+            body: JSON.stringify({
+                query: `query Version($projectId: String!){version(projectId: $projectId)}`,
+                variables: {
+                    projectId: context.projectId!,
+                },
+            }),
+        });
+
+        const { data } = await result.json();
+
+        this.userId.next(data.version);
+    }
+
+    countUp(): void {
+        this.microFrontendService.countUp();
+    }
 }
