9a02e4d033161cc50a99c76559d5726f7214e29b
Daniel Campos Olivares <daniel.campos.olivares@sap.com>
2023-06-30 17:04:57 +0200

    fix: Run Script on GCLOUD oficial container

--- .pipeline/scripts/gcr-auth.sh
+++ .pipeline/scripts/gcr-auth.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-echo "Transforming GCP credentials to file"
+echo "1. Transforming GCP credentials to file"
 echo -n $GCP_JSON_KEY_BASE64 | base64 -d > service-account.json
 
-cat service-account.json
-
-echo "Retrieving JWT id_token from GCP using given file"
+echo "2. Retrieving JWT id_token from GCP using given file"
 gcloud auth activate-service-account --key-file=./service-account.json
 export GCP_REGISTRY_REFERENCE_TOKEN=$(gcloud auth print-access-token gcr-pull@sap-gcp-dxp-prod.iam.gserviceaccount.com)
 
-echo "$GCP_REGISTRY_REFERENCE_TOKEN"
-
-echo "Substituting the entry in yarnrc by actual token"
+echo "3. Substituting the entry in yarnrc by actual token"
 sed -i -e "s#\"\${GCP_REGISTRY_REFERENCE_TOKEN-}\"#$GCP_REGISTRY_REFERENCE_TOKEN#g" .yarnrc.yml
+
+cat .yarnrc.yml
