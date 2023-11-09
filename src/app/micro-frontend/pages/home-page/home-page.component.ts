fd61b77b3ebeae4a975b55809ca275f2eb7bde11
Abhijith Ravindra <abhijith.ravindra@sap.com>
2023-11-09 12:02:05 +0100

    Change Landing page to Pipeline setup page (#124)
    * downgrade fundamental styles
    
    * create pipeline if it does not exist
    
    * remove start and single services, switch case in template
    
    * check if pipeline has resourceRefs
    
    * remove unused components
    
    * update import paths
    
    * revert changes to generated

--- src/app/micro-frontend/pages/home-page/home-page.component.ts
+++ src/app/micro-frontend/pages/home-page/home-page.component.ts
@@ -1,74 +1,75 @@
-import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core'
+import { ChangeDetectionStrategy, Component, HostListener, OnInit } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { AsyncPipe, CommonModule } from '@angular/common'
 import { PlatformDynamicPageModule } from '@fundamental-ngx/platform/dynamic-page'
 import { FundamentalNgxCoreModule, SvgConfig } from '@fundamental-ngx/core'
 import { ApolloModule } from 'apollo-angular'
 import { PipelineService } from '../../services/pipeline.service'
-import { StartComponent } from '../start/start.component'
-import { Observable, debounceTime } from 'rxjs'
+import { BehaviorSubject, debounceTime, firstValueFrom, Observable } from 'rxjs'
 import { PipelineComponent } from '../pipeline/pipeline.component'
-import { SingleServicesComponent } from '../single-services/single-services.component'
 import { Pipeline } from 'src/app/types'
 import { DebugModeService } from '../../services/debug-mode.service'
 import { tntSpotSecret } from '../../../../assets/ts-svg/tnt-spot-secret'
 import { AuthorizationModule } from '@dxp/ngx-core/authorization';
-import { LuigiClient, DxpLuigiContextService } from '@dxp/ngx-core/luigi'
+import { DxpLuigiContextService, LuigiClient } from '@dxp/ngx-core/luigi'
 import { PipelineType } from 'src/generated/graphql'
 
 
-
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'],
-  standalone: true,
-  imports: [
-    AuthorizationModule,
-    PlatformDynamicPageModule,
-    AsyncPipe,
-    FundamentalNgxCoreModule,
-    ApolloModule,
-    HttpClientModule,
-    StartComponent,
-    PipelineComponent,
-    SingleServicesComponent,
-    CommonModule,
-  ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.css'],
+    standalone: true,
+    imports: [
+        AuthorizationModule,
+        PlatformDynamicPageModule,
+        AsyncPipe,
+        FundamentalNgxCoreModule,
+        ApolloModule,
+        HttpClientModule,
+        PipelineComponent,
+        CommonModule,
+    ],
 })
-export class HomePageComponent {
-  pageTitle = 'CI/CD'
-  pipelineType = PipelineType
+export class HomePageComponent implements OnInit {
+    pageTitle = 'CI/CD'
+    pipelineType = PipelineType
+    watch$: Observable<Pipeline>
+    pipelineAvail = new BehaviorSubject<boolean>(false);
+    readonly spotConfig: SvgConfig = {
+        spot: {
+            file: tntSpotSecret,
+            id: 'tnt-Spot-Secrets-alternate',
+        },
+    };
 
-  constructor(
-    private readonly pipelineService: PipelineService,
-    private readonly debugModeService: DebugModeService,
-    private readonly luigiClient: LuigiClient,
-    private readonly context: DxpLuigiContextService
-  ) { }
+    constructor(
+        private readonly pipelineService: PipelineService,
+        private readonly debugModeService: DebugModeService,
+        private readonly luigiClient: LuigiClient,
+        private readonly context: DxpLuigiContextService
+    ) {
+    }
 
-  watch$: Observable<Pipeline>
+    async ngOnInit(): Promise<void> {
+        await this.initializePipeline();
+        this.watch$ = this.pipelineService.watchPipeline().pipe(debounceTime(50))
+    }
 
-  ngOnInit(): void {
-    this.watch$ = this.pipelineService.watchPipeline().pipe(debounceTime(50))
-  }
+    navigateToProjectMembers() {
+        this.luigiClient.linkManager().navigate(
+            `/projects/${this.context.getContext().projectId}/members`
+        );
+    }
 
-  navigateToProjectMembers() {
-    this.luigiClient.linkManager().navigate(
-      `/projects/${this.context.getContext().projectId}/members`
-    );
-  }
+    @HostListener('document:keydown.control.d', ['$event'])
+    handleCtrlDEvent(_: KeyboardEvent) {
+        this.debugModeService.toggleDebugMode()
+    }
 
-  readonly spotConfig: SvgConfig = {
-    spot: {
-      file: tntSpotSecret,
-      id: 'tnt-Spot-Secrets-alternate',
-    },
-  };
-
-  @HostListener('document:keydown.control.d', ['$event'])
-  handleCtrlDEvent(_: KeyboardEvent) {
-    this.debugModeService.toggleDebugMode()
-  }
+    private async initializePipeline(): Promise<void> {
+        await firstValueFrom(this.pipelineService.createPipeline(this.pipelineType.FullPipeline).pipe(debounceTime(100)));
+        this.pipelineAvail.next(true);
+    }
 }
