1569ec46bfa7d1de757684cbe073ecdf77c3f0ae
Julia Baum <julia.baum@sap.com>
2025-05-23 16:16:00 +0200

    fix cxone extension id (#675)

--- projects/ui/src/app/micro-frontend/services/extension.types.ts
+++ projects/ui/src/app/micro-frontend/services/extension.types.ts
@@ -12,7 +12,7 @@ export enum Extensions {
   VAULT = 'hashicorp-vault-vault-tools-sap',
   CHECKMARX = 'checkmarx',
   MEND = 'mend',
-  CHECKMARX_ONE = 'checkmarx-one',
+  CHECKMARX_ONE = 'cxone',
   AZURE_PIPELINES = 'azure-pipelines',
   GITHUB_TOOLS = 'git-hub-github-tools-sap',
   GITHUB_ACTIONS_TOOLS_SAP = 'git-hub-actions-github-tools-sap',
